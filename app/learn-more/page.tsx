1f9dc11429da8155fba968d70b873095d02d6e9d
Enpersonn <nicklas.b.overjordet@gmail.com>
2024-10-04 08:23:28 +0200

    Worked more on learn more page

--- app/learn-more/page.tsx
+++ app/learn-more/page.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/input/Button";
 import { SquareArrowOutUpRight } from "lucide-react";
 import Link from "next/link";
 
@@ -7,11 +8,12 @@ import Link from "next/link";
 export default function LearnMore() {
     return (
         <div className=" grid grid-cols-12 ">
-            <header className=" col-start-2 my-5 col-span-10">
-            <h1>Learn More</h1>
+            <header className=" col-start-2 my-5 col-span-10 flex  ">
+                <Button href="/">GoBack</Button>
             </header>
             <main className=" col-start-3 col-span-8">
 
+            <h1>Learn More</h1>
             <ul>
                 <li><Link href="https://nextjs.org/docs" className=" flex gap-2 items-center">Next.js Docs <SquareArrowOutUpRight className=" size-4" /></Link></li>
                 <li><Link href="https://tailwindcss.com/docs/installation" className=" flex gap-2 items-center">Tailwind CSS Docs <SquareArrowOutUpRight className=" size-4" /></Link></li>
